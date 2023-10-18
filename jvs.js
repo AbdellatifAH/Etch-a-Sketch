bff4f8a1fe8caa2d769c7254b491852108e758c9
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-10-18 21:46:29 +0300

    Cells background color will change when mouse enters a cell

--- jvs.js
+++ jvs.js
@@ -1,6 +1,6 @@
 const container = document.getElementById("container");
 
-let size = 16
+let size = 4
 
 for(let i = 0;i< size*size;i++){
     const cell= document.createElement("div")
@@ -9,4 +9,13 @@ for(let i = 0;i< size*size;i++){
     cell.style.width = 400 / size + "px";
     cell.style.height = 400 / size + "px";
     container.append(cell)
+    cell.addEventListener('mouseenter',changeCellColor);
+}
+
+function changeCellColor(){
+    let r=Math.floor(Math.random() * 256);
+    let g=Math.floor(Math.random() * 256);
+    let b=Math.floor(Math.random() * 256);
+    this.style.backgroundColor=`rgb(${r},${g},${b})`;
+    this.removeEventListener('mouseenter',changeCellColor);
 }
