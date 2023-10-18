825f4766064a73f2a59a8c441e859ef8a98f51f4
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-10-18 22:20:13 +0300

    Added a button to change size based on user input

--- jvs.js
+++ jvs.js
@@ -1,15 +1,21 @@
 const container = document.getElementById("container");
 
-let size = 4
+function createGrid(size=16){
 
-for(let i = 0;i< size*size;i++){
-    const cell= document.createElement("div")
-    cell.style.boxSizing="border-box"
-    cell.style.border="1px solid"
-    cell.style.width = 400 / size + "px";
-    cell.style.height = 400 / size + "px";
-    container.append(cell)
-    cell.addEventListener('mouseenter',changeCellColor);
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+        }
+
+    for(let i = 0;i< size*size;i++){
+        const cell= document.createElement("div")
+        cell.style.boxSizing="border-box"
+        cell.style.border="1px solid"
+        cell.style.width = 500 / size + "px";
+        cell.style.height = 500 / size + "px";
+        container.append(cell)
+        cell.addEventListener('mouseenter',changeCellColor);
+    }
+
 }
 
 function changeCellColor(){
@@ -19,3 +25,18 @@ function changeCellColor(){
     this.style.backgroundColor=`rgb(${r},${g},${b})`;
     this.removeEventListener('mouseenter',changeCellColor);
 }
+
+createGrid()
+
+function getUserInput(){
+    let d
+    do{
+    d= prompt("Enter Desired Grid Size (less than 100)");
+    }while(d>100);
+    createGrid(d);
+}
+
+const btn = document.querySelector("#btn")
+btn.addEventListener('click', getUserInput );
+
+
